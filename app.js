b0b2b10c9627590b9f9ef130d7ad6b24a1dc45bc
mugala <mugalalevi21@gmail.com>
2018-08-09 12:13:18 +0300

    Add cols and append This commit will add cols using innerHTML and append the list to row

--- app.js
+++ app.js
@@ -16,7 +16,15 @@ UI.prototype.addBookToList = function(book){
   // Create tr element
   const row = document.createElement('tr');
 
-  console.log(row);
+  // Insert cols
+  row.innerHTML = `
+  <td>${book.title}</td>
+  <td>${book.author}</td>
+  <td>${book.isbn}</td>
+  <td><a href = '#' class = 'delete'>X</a></td>
+  `;
+
+  list.appendChild(row);
 
 }
 // Event Listeners
