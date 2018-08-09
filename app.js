3052f952622b5685350b8818c1354b4d8abfc420
mugala <mugalalevi21@gmail.com>
2018-08-09 11:52:23 +0300

    Variables for author isbn and title This commit will create the variables for the author isbn and the title fields

--- app.js
+++ app.js
@@ -11,6 +11,11 @@ function UI(){}
 
 // Event Listeners
 document.getElementById('book-form').addEventListener('submit', function(e){
+  const title = document.getElementById('title').value,
+        author = document.getElementById('author').value,
+        isbn = document.getElementById('isbn').value;
+    
+  console.log(title,author,isbn);
 
   e.preventDefault();
 });
