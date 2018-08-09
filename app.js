de2fb5e799d8e0abe943cfe837e0a1dd52b378f3
mugala <mugalalevi21@gmail.com>
2018-08-09 11:48:28 +0300

    Book and UI constructors This commit will create the UI and the Book constructors ES5 style

---
+++ app.js
@@ -0,0 +1,17 @@
+// Book constructor
+function Book (title,author,isbn){
+  this.title = title;
+  this.author = author;
+  this.isbn = isbn;  
+}
+
+// UI constructor
+
+function UI(){}
+
+// Event Listeners
+document.getElementById('book-form').addEventListener('submit', function(e){
+
+  e.preventDefault();
+});
+
